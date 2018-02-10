3257206d79323c11af10f967fdf5dbc9965d0cbb
Cuttlefish <zh4n@live.co.uk>
2018-02-10 18:52:23 +0000

    Basic Collision
    bounding box collision

--- Shooter/Shooter/Game1.cs
+++ Shooter/Shooter/Game1.cs
@@ -123,7 +123,7 @@ namespace Shooter
             bgLayer1.Update();
             bgLayer2.Update();
             UpdateEnemies(gameTime);
-
+            UpdateCollision();
 
             base.Update(gameTime);
         }
@@ -209,7 +209,46 @@ namespace Shooter
             // Add the enemy to the active enemies list
             enemies.Add(enemy);
         }
+        private void UpdateCollision()
+        {
+            // Use the Rectangle's built-in intersect function to 
+            // determine if two objects are overlapping
+            Rectangle rectangle1;
+            Rectangle rectangle2;
 
+            // Only create the rectangle once for the player
+            rectangle1 = new Rectangle((int)player.Position.X,
+            (int)player.Position.Y,
+            player.Width,
+            player.Height);
+
+            // Do the collision between the player and the enemies
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                rectangle2 = new Rectangle((int)enemies[i].Position.X,
+                (int)enemies[i].Position.Y,
+                enemies[i].Width,
+                enemies[i].Height);
+
+                // Determine if the two objects collided with each
+                // other
+                if (rectangle1.Intersects(rectangle2))
+                {
+                    // Subtract the health from the player based on
+                    // the enemy damage
+                    player.Health -= enemies[i].Damage;
+
+                    // Since the enemy collided with the player
+                    // destroy it
+                    enemies[i].Health = 0;
+
+                    // If the player health is less than zero we died
+                    if (player.Health <= 0)
+                        player.Active = false;
+                }
+
+            }
+        }
         private void UpdateEnemies(GameTime gameTime)
         {
             // Spawn a new enemy enemy every 1.5 seconds
